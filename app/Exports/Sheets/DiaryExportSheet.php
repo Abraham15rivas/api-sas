8193a6f2e787cf1c83c71d8a16bbf0fdcd3db548
Abraham15RJ <abraham@ocularsolution.com>
2022-09-20 01:00:56 -0400

    added style report excel

--- app/Exports/Sheets/DiaryExportSheet.php
+++ app/Exports/Sheets/DiaryExportSheet.php
@@ -3,14 +3,17 @@
 namespace App\Exports\Sheets;
 
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Maatwebsite\Excel\Events\AfterSheet;
 use Maatwebsite\Excel\Concerns\{
     FromArray,
     WithTitle,
     WithMapping,
     WithHeadings,
     WithStrictNullComparison,
-    ShouldAutoSize,
-    WithStyles
+    // ShouldAutoSize,
+    WithStyles,
+    WithCustomStartCell,
+    WithEvents
 };
 
 class DiaryExportSheet implements 
@@ -19,16 +22,19 @@ class DiaryExportSheet implements
     WithMapping, 
     WithHeadings, 
     WithStrictNullComparison,
-    ShouldAutoSize,
-    WithStyles   
+    // ShouldAutoSize,
+    WithStyles,
+    WithCustomStartCell,
+    WithEvents
 {
-    private $dairies, $title, $headers;
+    private $diaries, $title, $headers, $mainTitle;
 
-    public function __construct($dairies = null, $title = null, $headers)
+    public function __construct($diaries = null, $title = null, $headers, $mainTitle = null)
     {
-        $this->dairies    = $dairies;
+        $this->diaries    = $diaries;
         $this->title      = $title;
         $this->headers    = $headers;
+        $this->mainTitle  = $mainTitle;
     }
 
     /**
@@ -36,7 +42,7 @@ class DiaryExportSheet implements
     */
     public function array(): array
     {
-        return $this->dairies;
+        return $this->diaries;
     }
     
     public function title(): string
@@ -59,22 +65,119 @@ class DiaryExportSheet implements
     */
     public function headings(): array
     {
-        return $this->headers;
+        return [
+            ['AGENDA SEMANAL'],
+            ['MINISTERIO DEL PODER POPULAR PARA LA CIENCIA Y TECNOLOGIA'],
+            [$this->mainTitle],
+            ['empty'],
+            $this->headers
+        ];
+    }
+
+    public function startCell(): string
+    {
+        return 'A3';
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                // Titulo
+                $event->sheet->mergeCells('A3:K3');
+                $event->sheet->mergeCells('A4:K4');
+                $event->sheet->mergeCells('A5:K5');
+
+                // Height celda (Altura)
+                $event->sheet->getDelegate()->getRowDimension('7')->setRowHeight(100);
+
+                for ($i = 8; $i < (count($this->diaries) + 8); $i++) {
+                    $event->sheet->getDelegate()->getRowDimension("$i")->setRowHeight(100);
+                }
+
+                // No funcionan cuando la clase ShouldAutoSize esta implementada
+                $event->sheet->getDelegate()->getColumnDimension('A')->setWidth(8);
+                $event->sheet->getDelegate()->getColumnDimension('B')->setWidth(20);
+                $event->sheet->getDelegate()->getColumnDimension('C')->setWidth(50);
+                $event->sheet->getDelegate()->getColumnDimension('D')->setWidth(40);
+                $event->sheet->getDelegate()->getColumnDimension('E')->setWidth(80);
+                $event->sheet->getDelegate()->getColumnDimension('F')->setWidth(20);
+                $event->sheet->getDelegate()->getColumnDimension('G')->setWidth(25);
+                $event->sheet->getDelegate()->getColumnDimension('H')->setWidth(25);
+                $event->sheet->getDelegate()->getColumnDimension('I')->setWidth(20);
+                $event->sheet->getDelegate()->getColumnDimension('J')->setWidth(40);
+                $event->sheet->getDelegate()->getColumnDimension('K')->setWidth(40);
+            },
+        ];
     }
 
     public function styles(Worksheet $sheet)
     {
-        $data[1] = ['font' => [ 'bold' => true ]];
-        $data['A'] = ['alignment' => ['horizontal' => 'center']];
-        $data['F'] = ['font' => ['bold' => true]];
-        $data['G'] = ['font' => [
+        
+        $countCells = (count($this->diaries) + 7);
+        $range      = "A8:K$countCells";
+        $rangeBorder= "A7:K$countCells";
+
+        $data['3:5'] = [
+            'font' => [
+                'name' => 'Comic Sans MS',
+                'size' => '20',
                 'bold' => true,
-                'color' => ['argb' => '000000']
+                'color' => array('rgb' => '000000')
+            ],
+            'alignment' => [
+                'horizontal' => 'center',
+                'vertical' => 'center'
             ]
         ];
-        $data['H'] = ['font' => [
+
+        $data['6'] = [
+            'font' => [
+                'color' => array('rgb' => 'ffffff')
+            ]
+        ];
+
+        $data["$range"] = [
+            'font' => [
+                'name' => 'Comic Sans MS',
                 'bold' => true,
-                'color' => ['argb' => '000000']
+                'color' => array('rgb' => '000000')
+            ]
+        ];
+
+        $data['7'] = [
+            'fill' => [
+                'fillType' => 'solid',
+                'color' => array('rgb' => "14B8A6")
+            ],
+            'font' => [
+                'name' => 'Comic Sans MS',
+                'bold' => true,
+                'color' => array('rgb' => 'ffffff')
+            ]
+        ];
+
+        $data['L'] = [
+            'font' => [
+                'color' => array('argb' => 'ffffff')
+            ]
+        ];
+
+        $data["$rangeBorder"] = [
+            'alignment' => [
+                'horizontal' => 'center',
+                'vertical' => 'center'
+            ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    'color' => array('rgb' => '000000'),
+                ],
             ]
         ];
 
